7a91abc99ee6e6bf5491dcbead474df27c605f53
WR.dong <527184984@qq.com>
2021-04-01 18:34:22 +0800

    在公共路由增加参数获取

--- web/api/app/Http/Controllers/Controller.php
+++ web/api/app/Http/Controllers/Controller.php
@@ -6,5 +6,29 @@ use Laravel\Lumen\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    //
+
+    protected $inputData;
+    protected $perPage = 10;
+
+    public function __construct()
+    {
+        $this->inputData = app()->request->all();
+    }
+
+    /**
+     * 获取请求参数
+     *
+     * @param null $key 获取指定参数，null 时获取所有参数
+     * @param null $default
+     * @return mixed
+     * @author   WR.dong
+     */
+    protected function input($key = null, $default = '')
+    {
+        $data = data_get($this->inputData, $key, $default);
+        if (is_string($data)) {
+            return trim($data);
+        }
+        return $data;
+    }
 }
